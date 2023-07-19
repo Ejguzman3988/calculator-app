b9a053f9e69dc39b1bfe6c8c2db374e11b919c2f
Eriberto <eriberto.guzman@outlook.com>
2023-07-18 22:14:27 -0400

    quick fix to order of numbers

--- app/(components)/ButtonBox.js
+++ app/(components)/ButtonBox.js
@@ -1,12 +1,12 @@
 import Button from "./Button";
 
 const ButtonBox = () => {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
-    <Button content={num} />
-  ));
+  const numbers = [0, 3, 2, 1, 6, 5, 4, 9, 8, 7]
+    .reverse()
+    .map((num) => <Button content={num} />);
 
   return (
-    <div className="border p-10 rounded-md  grid grid-cols-4 gap-4">
+    <div className="border p-10 rounded-md grid grid-cols-3 gap-x-1 gap-y-4 reverse grid-flow-dense">
       {numbers}
     </div>
   );
